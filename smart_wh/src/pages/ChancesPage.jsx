69b4a485affb2b83da15c9acd7c45b8bc5f11458
Diyorbek <90106883+iDev07@users.noreply.github.com>
2022-06-05 00:13:38 +0500

    Latest changes pushed

--- smart_wh/src/pages/ChancesPage.jsx
+++ smart_wh/src/pages/ChancesPage.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import Main from "../components/Chances/Ch_Main";
 import Ch_stats from "../components/Chances/Ch_stats";
+import Ch_stats2 from "../components/Chances/Ch_stats2";
+import Ch_Videos from "../components/Chances/Ch_Videos";
+import Footer from "../components/Footer/Footer";
 import Header from "../components/Header/Header";
 
 function ChancesPage() {
@@ -9,6 +12,9 @@ function ChancesPage() {
       <Header />
       <Main />
       <Ch_stats />
+      <Ch_Videos />
+      <Ch_stats2 />
+      <Footer />
     </div>
   );
 }
--- smart_wh/src/pages/HomePage.jsx
+++ smart_wh/src/pages/HomePage.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Header from "../components/Header/Header";
 import Automation from "../components/Home/Automation";
 import Carousel from "../components/Home/Carousel";
-import Footer from "../components/Home/Footer";
+import Footer from "../components/Footer/Footer";
 import Main from "../components/Home/Main";
 import MobileApp from "../components/Home/MobileApp";
 import Opportunites from "../components/Home/Opportunites";
